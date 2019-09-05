afcf87fc8882299c6475ee187349c2b78b7f395f
Vinaysachdeva <39600438+Vinaysachdeva@users.noreply.github.com>
2019-09-05 16:34:14 +0530

    Update Untitled-1.c

--- Untitled-1.c
+++ Untitled-1.c
@@ -28,19 +28,19 @@ int allocator(int i, int j, int ctr){
         return 0;
     if(!maze[i][j].up && maze[i-1][j].val>ctr+1){
         maze[i-1][j].val=ctr+1;
-        func(i-1, j, ctr+1);
+        allocator(i-1, j, ctr+1);
     }
     if(!maze[i][j].down && maze[i+1][j].val>ctr+1){
         maze[i+1][j].val=ctr+1;
-        func(i+1,j, ctr+1);
+        allocator(i+1,j, ctr+1);
     }
     if(!maze[i][j].left && maze[i][j-1].val>ctr+1){
         maze[i][j-1].val=ctr+1;
-        func(i, j-1, ctr+1);
+        allocator(i, j-1, ctr+1);
     }
     if(!maze[i][j].down && maze[i][j+1].val>ctr+1){
         maze[i][j+1].val=ctr+1;
-        func(i, j+1,ctr+1);
+        allocator(i, j+1,ctr+1);
     }
     return 1;
 }
